576de505d6a9b2ae39e27617c9be62a422c35aec
Szymon Szaminko <sz.szaminko@gmail.com>
2021-10-11 21:35:16 +0200

    Create new div section and  CSS file to currency charts and testing scripts

--- currency-exchange/scripts/script.js
+++ currency-exchange/scripts/script.js
@@ -6,8 +6,21 @@ function updateValue() {
     valueOutput.value = valueInput.value;
 }
 
+const euro = {
+    name: "Euro",
+    course: {
+        changeTo: {
+            euro: 1,
+            zloty: 4.15,
+            dollar: 2.5,
+            pfudn: 3.3
+        }
+    }
+}
+
 valueInput.addEventListener('input', function() {
     console.log(valueInput.value);
-    valueOutput.value = valueInput.value;
+    let course = euro.course.changeTo.zloty;
+    valueOutput.value = parseInt(valueInput.value) * course;
 
 });
